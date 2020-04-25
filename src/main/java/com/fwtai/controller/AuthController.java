ca59dc95b4bd1b5b700cccc56f5f71db5e484608
gzstyp <444141300@qq.com>
2020-04-25 13:16:13 +0800

    Initial commit

---
+++ src/main/java/com/fwtai/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.fwtai.controller;
+
+import com.fwtai.entity.User;
+import com.fwtai.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody Map<String,String> registerUser){
+        final User user = new User();
+        user.setUsername(registerUser.get("username"));
+        user.setPassword(bCryptPasswordEncoder.encode(registerUser.get("password")));
+        user.setRole("ROLE_USER");
+        userService.save(user);
+        return "success";
+    }
+}
