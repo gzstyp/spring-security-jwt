ba25117c67d58eea47801d0269e5bbd0480a95b9
gzstyp <444141300@qq.com>
2020-04-27 22:28:41 +0800

    ServletException

--- src/main/java/com/fwtai/JWTAuthenticationPoint.java
+++ src/main/java/com/fwtai/JWTAuthenticationPoint.java
@@ -4,14 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class JWTAuthenticationPoint implements AuthenticationEntryPoint{
 
-    public void commence(final HttpServletRequest request,final HttpServletResponse response,final AuthenticationException authException) throws IOException, ServletException{
+    public void commence(final HttpServletRequest request,final HttpServletResponse response,final AuthenticationException authException) throws IOException{
         response.setContentType("text/html;charset=UTF-8");
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         String reason = "统一处理，原因：" + authException.getMessage();
